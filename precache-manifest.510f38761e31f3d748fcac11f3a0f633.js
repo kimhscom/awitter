bff1faaf6aec4b684b2fe5d6f9a4b3bbdb7e817c
kimhscom <47877911+kimhscom@users.noreply.github.com>
2020-09-20 00:26:24 +0900

    Updates

---
+++ precache-manifest.510f38761e31f3d748fcac11f3a0f633.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d6ef90c5d08033b0747d6723990ab0c",
+    "url": "/awitter/index.html"
+  },
+  {
+    "revision": "6066725de8097057e82b",
+    "url": "/awitter/static/css/main.70715651.chunk.css"
+  },
+  {
+    "revision": "9adc6a26dd5a35920230",
+    "url": "/awitter/static/js/2.d2ffae6a.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/awitter/static/js/2.d2ffae6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6066725de8097057e82b",
+    "url": "/awitter/static/js/main.5f8f2d42.chunk.js"
+  },
+  {
+    "revision": "285150e5afc7ff9b35e6",
+    "url": "/awitter/static/js/runtime-main.001eb6ae.js"
+  }
+]);
